43947ba924aa262ff399b75632ae43c4f6ed3726
A0H1-PY <abdex7741@gmail.com>
2026-07-16 18:45:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ CLIENT_CONFIG = {
         'auth_uri': "https://accounts.google.com/o/oauth2/auth",
         'token_uri': "https://oauth2.googleapis.com/token",
         'auth_provider_x509_cert_url': "https://www.googleapis.com/oauth2/v1/certs",
-        'redirect_uris': ["https://psfuturemail.onrender.com"]
+        'redirect_uris': ["https://psfuturemail.onrender.com/oauth2callback"]
     }
 }
 
